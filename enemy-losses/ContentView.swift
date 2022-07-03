3ce743e25533ade9b8c801a58a6bb872c9a07c7c
Kazzer <kazzer.nd@gmail.com>
2022-07-03 21:39:57 +0300

    Initial Commit

---
+++ enemy-losses/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  enemy-losses
+//
+//  Created by Eugene Ned on 03.07.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ enemy-losses/enemy_lossesApp.swift
@@ -0,0 +1,17 @@
+//
+//  enemy_lossesApp.swift
+//  enemy-losses
+//
+//  Created by Eugene Ned on 03.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct enemy_lossesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
